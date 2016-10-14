f415aa086cf7e9ae5acef2cc28e98f037b37bd9c
Ed <ed@xmos.com>
2016-10-14 16:18:47 +0100

    Revert directory layout of host files so it doesn't break xvsm apps (we are not changing major version here)

---
+++ tests/test_usb_end_to_end_hardware.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+import xmostest
+import re
+import os.path
+
+class device_control_endtoend_tester(xmostest.Tester):
+    # This checks for errors reported by all of the processes run
+    # during the test. If no errors are seen the test will be marked as a pass.
+
+
+    def __init__(self, transport, host_app_name, device_app_name):
+        super(device_control_endtoend_tester, self).__init__()
+        self.product = "lib_device_control"
+        self.group = "lib_device_control_tests"
+        self.test = test
+        self.config = {'transport':transport}
+        self.register_test(self.product, self.group, self.test, self.config)
+
+
+    def record_failure(self, failure_reason):
+        # Append a newline if there isn't one already
+        if not failure_reason.endswith('\n'):
+            failure_reason += '\n'
+        self.failures.append(failure_reason)
+        print ("Failure reason: %s" % failure_reason), # Print without newline
+        self.result = False
+
+
+    def run(self,
+            device_job_output,
+            host_job_output):
+        self.result = True
+        self.failures = []
+
+        # Check for any errors
+        for line in (device_job_output
+                     + host_job_output
+                     ):
+            if re.match('.*ERROR|.*error|.*Error|.*Problem|.*ruined|could not',
+                        line):
+                self.record_failure(line)
+
+        output = {'device_job_output':''.join(dut_programming_job_output),
+                  'host_job_output':''.join(control_job_output)
+                  }
+
+        if not self.result:
+            output['failures'] = ''.join(self.failures)
+        
+        xmostest.set_test_result(self.product,
+                                 self.group,
+                                 self.test,
+                                 self.config,
+                                 self.result,
+                                 env={},
+                                 output=output)
+
+
+def runtest():
+
+    # Check if the test is running in an environment with hardware resources
+    # i.e. if not running from usb audio view it will quit. lib_device_control 
+    # is in the usb audio view so it will build
+    args = xmostest.getargs()
+    if not args.remote_resourcer:
+      # Abort the test
+      print 'remote resourcer not avaliable'
+      return
+
+    testlevel = 'smoke'
+    tester.set_min_testlevel(testlevel)
+
+    device_app_name = 'usb_end_to_end_hardware/bin/usb_end_to_end_hardware.xe'.format()
+    host_app_name = 'usb_end_to_end_hardware_host/bin/usb_end_to_end_hardware_host.out'
+
+    board = 'uac2_xcore200_mc_testrig_os_x_11'
+
+    # Setup the tester which will determine and record the result
+    tester = xmostest.CombinedTester(2, device_control_endtoend_tester("usb",
+                                                       host_app_name, device_app_name))
+
+    # Get the hardware resources to run the test on
+    resources = None
+    try:
+        resources = xmostest.request_resource(board, tester)
+    except xmostest.XmosTestError:
+        print "Unable to find required board %s required to run test" % board
+        tester.shutdown()
+        return
+
+
+    # Start the xCORE DUT
+    device_job = xmostest.run_on_xcore(resources['dut'], device_app_name,
+                                        do_xe_prebuild = True,
+                                        tester = tester[0],
+                                        enable_xscope=True, 
+                                        xscope_handler=None, 
+                                        timeout=60,
+                                        build_env = env)
+
+
+    # Start the control app
+    host_job = xmostest.run_on_pc(resources['host_primary'],
+                                     ['python', host_app_name,
+                                      'agc_on', '0',
+                                      'doa_dir', str(doa_dir)],
+                                     tester = tester[1],
+                                     timeout = 5,
+                                     initial_delay = 5)
+                                      #start_after_completed = [device_job]
+    
+    xmostest.complete_all_jobs()
---
+++ tests/usb_end_to_end_hardware_host/src/pause.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2016, XMOS Ltd, All rights reserved
+#ifndef __pause_h__
+#define __pause_h__
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#ifdef _WIN32
+
+static void pause_short(void)
+{
+  Sleep(1);
+}
+
+static void pause_long(void)
+{
+  Sleep(1000);
+}
+
+#else
+
+static void pause_short(void)
+{
+  usleep(100000);
+}
+
+static void pause_long(void)
+{
+  sleep(1);
+}
+
+#endif // WIN32
+
+#endif // __pause_h__
