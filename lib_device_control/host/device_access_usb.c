9f0a999f74ce8d14adf6edc2bad04180d296bed9
lucianomartin <xmos@tini.xmos.local>
2018-05-18 14:32:04 +0100

    Silence warning for Windows

--- lib_device_control/host/device_access_usb.c
+++ lib_device_control/host/device_access_usb.c
@@ -33,6 +33,8 @@ static const int sync_timeout_ms = 100;
 void debug_libusb_error(int err_code)
 {
 #if defined _WIN32
+  /* err_code is not used in Windows platforms */
+  (void) err_code;
   printf("libusb_control_transfer returned %s\n", usb_strerror());
 #elif defined __APPLE__
   printf("libusb_control_transfer returned %s\n", libusb_error_name(err_code));
