b63ab43ec4a1d1838231f8143862bd95f9adc2bc
Larry Snizek <larry@xmos.com>
2019-05-09 12:00:25 +0100

    Android app example - use precompiled libusb

--- examples/usb_android_java_app/host/jni/Android.mk
+++ examples/usb_android_java_app/host/jni/Android.mk
@@ -1,13 +1,17 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libusb1.0
+LOCAL_SRC_FILES := ../prebuilt/$(TARGET_ARCH_ABI)/libusb1.0.so
+LOCAL_EXPORT_C_INCLUDES := ../prebuilt
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := app
 LOCAL_SRC_FILES := app.c
-LOCAL_SHARED_LIBRARIES += libusb1.0
 LOCAL_LDLIBS := -llog
+LOCAL_SHARED_LIBRARIES := libusb1.0
 LOCAL_C_INCLUDES := ../.. \
   ../../../../../lib_device_control/lib_device_control/src \
   ../../../../../lib_device_control/lib_device_control/api
-
 include $(BUILD_SHARED_LIBRARY)
-include ../../../../../libusb/android/jni/libusb.mk # TODO precompiled
