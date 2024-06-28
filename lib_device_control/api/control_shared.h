d74a09f7fb7096e288aff742612b0b488b88988a
Luciano Martin <lucianom@xmos.com>
2024-06-28 12:21:49 +0100

    Remove unnecessary code

--- lib_device_control/api/control_shared.h
+++ lib_device_control/api/control_shared.h
@@ -27,7 +27,6 @@
 typedef uint8_t control_resid_t;
 typedef uint8_t control_cmd_t;
 typedef uint8_t control_version_t;
-typedef uint8_t control_status_t;
 typedef uint8_t control_ret_t;
 /**@}*/
 
@@ -35,26 +34,13 @@ typedef uint8_t control_ret_t;
  * This type enumerates the possible outcomes from a control transaction.
  *
  */
-/* TODO: Some of the enum values below should be used
-    when https://xmosjira.atlassian.net/browse/LSM-71 is addressed */
 enum control_ret_values { /*This looks odd but helps us force byte enum */
     CONTROL_SUCCESS = 0,
     CONTROL_REGISTRATION_FAILED,
     CONTROL_BAD_COMMAND,
     CONTROL_DATA_LENGTH_ERROR,
     CONTROL_OTHER_TRANSPORT_ERROR,
-    CONTROL_BAD_RESOURCE,
-    CONTROL_MALFORMED_PACKET,
-    CONTROL_COMMAND_IGNORED_IN_DEVICE,
-    CONTROL_ERROR,
-
-    SERVICER_COMMAND_RETRY = 64,
-    SERVICER_WRONG_COMMAND_ID,
-    SERVICER_WRONG_COMMAND_LEN,
-    SERVICER_WRONG_PAYLOAD,
-    SERVICER_QUEUE_FULL,
-    SERVICER_RESOURCE_ERROR,
-
+    CONTROL_ERROR
 };
 
 /**@}*/
