e48562a8ca6e045d50bdd2349b69463ea16e87d4
Luciano Martin <lucianom@xmos.com>
2024-10-11 15:40:00 +0100

    Update function descriptions

--- tests/utils.py
+++ tests/utils.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python
-# Copyright 2024 XMOS LIMITED.
-# This Software is subject to the terms of the XMOS Public Licence: Version 1.
 import subprocess
 import logging
 from pathlib import Path
 
-def run_command(cmd, check_return_code=True, return_output=True, timeout_s=60):
+def run_command(cmd, check_return_code=True, return_output=False, timeout_s=60):
     """
     Runs a command in the shell.
 
     Parameters:
-    cmd (str): The command to run.
+    cmd (str or list): The command to run. Can be a string or a list of command arguments.
     check_return_code (bool): If True, raises a CalledProcessError if the command exits with a non-zero status.
     return_output (bool): If True, captures and returns the output of the command.
     timeout_s (int): The timeout in seconds for the command execution.
@@ -25,10 +22,11 @@ def run_command(cmd, check_return_code=True, return_output=True, timeout_s=60):
     Exception: If an unexpected error occurs.
     """
     cmd = cmd if isinstance(cmd, list) else cmd.split()
-    logging.debug(f"Running: {' '.join([ str(i) for i in cmd])}")
+    logging.debug(f"Running: {' '.join([str(i) for i in cmd])}")
     output = None
     try:
-        output = subprocess.run(cmd, capture_output=True, check=check_return_code, text=True, timeout=timeout_s).stdout
+        result = subprocess.run(cmd, capture_output=True, check=check_return_code, text=True, timeout=timeout_s)
+        output = result.stdout
         logging.debug(f"Command output: {output}")
         if return_output:
             return output
@@ -37,10 +35,8 @@ def run_command(cmd, check_return_code=True, return_output=True, timeout_s=60):
     except subprocess.CalledProcessError as e:
         logging.error(f"Error type: {type(e).__name__}")
         logging.error(f"Command failed with return code {e.returncode}")
-        logging.error(f"Output: {output}")
-        logging.error(f"Exception Output: {e.output}")
-        logging.error(f"Exception Standard Error: {e.stderr}")
-
+        logging.error(f"Output: {e.output}")
+        logging.error(f"Standard Error: {e.stderr}")
         raise
     except subprocess.TimeoutExpired as e:
         logging.error(f"Error type: {type(e).__name__}")
@@ -57,29 +53,31 @@ def run_command(cmd, check_return_code=True, return_output=True, timeout_s=60):
 
 def build_firmware(target, project_dir=Path("."), build_dir="build", check_return_code=True, timeout_s=60):
     """
-    Builds an XMOS firmware project using the `xmake` tool.
+    Builds the XMOS firmware project using CMake and xmake.
 
     Parameters:
-    project_dir (str): The path to the XMOS firmware project directory.
     target (str): The target to build.
-    build_dir (str): The path to the build directory. If None, the build directory is set to the project directory.
+    project_dir (Path or str): The path to the XMOS firmware project directory.
+    build_dir (Path or str): The path to the build directory. If None, the build directory is set to the project directory.
     check_return_code (bool): If True, raises a CalledProcessError if the command exits with a non-zero status.
-    return_output (bool): If True, captures and returns the output of the command.
     timeout_s (int): The timeout in seconds for the command execution.
 
     Returns:
-    Path to the .xe file of the built firmware.
-    """
+    Path: The path to the .xe file of the built firmware.
 
+    Raises:
+    FileNotFoundError: If the project directory or the expected .xe file does not exist.
+    exceptions raised by run_command()
+    """
     project_path = Path(project_dir)
     if not project_path.is_dir():
         raise FileNotFoundError(f"The directory {project_dir} does not exist.")
 
     build_path = Path(build_dir) if build_dir else project_path
     # Use a list below to avoid that the argument "Unix Makefiles" is split into two arguments.
-    cmd = [ "cmake", "-S", str(project_dir), "-G", "Unix Makefiles", "-B", str(project_dir/build_dir), "--fresh" ]
+    cmd = ["cmake", "-S", str(project_dir), "-G", "Unix Makefiles", "-B", str(project_dir/build_dir)]
     run_command(cmd, check_return_code, return_output=False, timeout_s=timeout_s)
-    cmd = f"xmake -C {project_dir / build_dir}"
+    cmd = f"xmake -C {project_dir / build_dir} -t {target}"
     run_command(cmd, check_return_code, return_output=False, timeout_s=timeout_s)
     expected_xe_file = project_dir / "bin" / f"{target}.xe"
     if not expected_xe_file.is_file():
@@ -91,10 +89,11 @@ def xsim_firmware(xe_file, check_return_code=True, return_output=True, timeout_s
     Simulates the execution of an XMOS executable file using the `xsim` tool.
 
     Parameters:
-    xe_file (str): The path to the XMOS executable (.xe) file.
+    xe_file (Path or str): The path to the XMOS executable (.xe) file.
     check_return_code (bool): If True, raises a CalledProcessError if the command exits with a non-zero status.
     return_output (bool): If True, captures and returns the output of the command.
     timeout_s (int): The timeout in seconds for the command execution.
+    sim_args (str): Additional arguments to pass to the `xsim` command.
 
     Returns:
     str: The standard output of the command if `return_output` is True.
@@ -102,8 +101,8 @@ def xsim_firmware(xe_file, check_return_code=True, return_output=True, timeout_s
 
     Raises:
     FileNotFoundError: If the .xe file does not exist.
+    exceptions raised by run_command()
     """
-
     xe_path = Path(xe_file)
     if not xe_path.is_file():
         raise FileNotFoundError(f"The file {xe_file} does not exist.")
