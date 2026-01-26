3b0ceaadeffeeb69e9a8494dadfd9d824b78a29a
Humphrey Bucknell <humphreybucknell@xmos.com>
2026-01-26 16:52:35 +0000

    Update source copyright

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2024-2025 XMOS LIMITED.
+# Copyright 2024-2026 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 import subprocess
 import logging
